cb3a9c37710d3d20d478edfdac9df29dfad01a77
arshilb4u <30823313+arshilb4u@users.noreply.github.com>
2017-08-11 23:13:39 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,11 @@ return htmlTamplates;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter',function(req,res){
+ counter=counter+1;
+ res.send(counter.tostring());
+});
 
 app.get('/article-one',function (req,res){
     res.send(createTemplate(articleone));
