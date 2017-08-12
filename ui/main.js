3f8e9bddb2eee16f939a3c45d20864aea0d702cd
arshilb4u <30823313+arshilb4u@users.noreply.github.com>
2017-08-11 22:55:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
 console.log('Loaded!');
 //change the text of main text div
 var element=document.getElementById=('main-text');
-element.innerHTML='New value'
+element.innerHTML='New value';
+
+
+//move the image
+var element=document.getElementById=('bhatia');
+img.onclick=function(){
+    img.style.marginleft='100px';
+    
+};
